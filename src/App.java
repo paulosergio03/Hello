7efd46a8e320017f24f4756a09813e7b323b0a7f
Paulo Sérgio <paulosergioadm9@gmail.com>
2025-07-01 21:18:42 -0300

    teste

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("teste 1 " + args[0]);
+        System.out.println("teste " + args[1]);
+       
+       
+    }
+}
